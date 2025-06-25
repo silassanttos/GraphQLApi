9f258c7358dacd0c5097df25331b2c27e768a586
silassanttos <silas_santos@outlook.com.br>
2025-06-24 23:27:23 -0300

    Inclusão Query Cliente

---
+++ GraphQL/Queries/ClienteQuery.cs
@@ -0,0 +1,21 @@
+﻿using GraphQLApi.Data;
+using GraphQLApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQLApi.GraphQL.Queries
+{
+    [ExtendObjectType(Name = "Query")]
+    public class ClienteQuery
+    {
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+
+        public async Task<IQueryable<Cliente>> GetClientesAsync([Service] IDbContextFactory<AppDbContext> contextFactory)
+        {
+            var context = contextFactory.CreateDbContext();
+            return await Task.FromResult(context.Clientes);
+        }
+
+    }
+}
